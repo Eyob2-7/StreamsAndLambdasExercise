7c3d9eb07360173ffbd2cea59888019da140b1b9
Eyob Mengistu <emengistu17@gmail.com>
2025-05-21 13:55:40 -0500

    Add new stuff

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -30,9 +30,12 @@ public class App {
         // 3. Create a list of just names
         System.out.println("\n=== Character Names ===");
         ArrayList<String> names = new ArrayList<String>();
+        // loop over my characters and get just the name and add it to the new list
         for (Character c : characters) {
             names.add(c.getName());
         }
+
+        //print out just the names by looping over the new list
         for (String name : names) {
             System.out.println(name);
         }
